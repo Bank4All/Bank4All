2608d1fcb466227a90cfbe1093f70131a2be72e8
Bank4All <94786830+Bank4All@users.noreply.github.com>
2021-11-29 13:02:43 +0100

    Create shadowbanking.hpp

---
+++ shadowbanking.hpp
@@ -0,0 +1,21 @@
+\\ _______ shadowbanking.hpp ________
+\\ 
+\\ Operations supervised CASHLESS
+
+\*
+
+Actor A , want to exchange his delivery of products against a pool of needs and promise of benefit future
+
+In traditionnal Banking system, we would use Cash as a pivot 
+In modern Banking , we would use an electronic secure treasure bond for each parties with a credit default score
+In shariah Banking, we would secure using a performance metric and american option model to desactivate the operations
+In bitcoin Banking, we would use a trusted third party cash holder for both parties to settle all the above 
+In shadow Banking, we automate performance of assets and upon agreement deliver differences or products
+
+For a regulator point of view, notional of operation is low, however leveraging is high and should be reported with a leverage of True Notional of Asset Value / Max( Notional of Cash, Min Asset Value )
+
+So as countrary as you people think, shadow banking is highly complex and require a dual acknowledgement of asset model value and a shared model
+
+As such , a shadow bank is responsible not for the price but for correct valuation and model of asset run during the operation
+
+*\
