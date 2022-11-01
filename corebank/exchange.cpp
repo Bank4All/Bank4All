51f9a821169edfd3a752642d55a33b4a2b7fd052
Bank4All <94786830+Bank4All@users.noreply.github.com>
2022-11-01 17:25:06 +0100

    Update exchange.cpp

--- corebank/exchange.cpp
+++ corebank/exchange.cpp
@@ -1,10 +1,10 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+// ---------------------------------------------------------------------------------
+// {S} ~  ..  _
+// {S} ScaIIneT company
+// (C) Copyright 2022
+// [L] License : Free of for french domain only
+// #M# Military limitation : Forbidden for missile and drone information processing
+// ---------------------------------------------------------------------------------
 
 #include <iostream>
 #include<ctime>
