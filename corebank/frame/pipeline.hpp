3b7570de0001c249ae8e206f3265fc34073c4e08
Bank4All <94786830+Bank4All@users.noreply.github.com>
2022-11-04 18:25:10 +0100

    Update pipeline.hpp

--- corebank/frame/pipeline.hpp
+++ corebank/frame/pipeline.hpp
@@ -1,1 +1,38 @@
-// scaiinet
+#include <iostream>
+#include <string>
+
+struct execute {
+    
+};
+
+template<class _D,int _S>
+struct pipeline 
+{
+    _D _data[_S>>4<<4];
+    size_t _index;
+    
+    pipeline() {
+        _index=0;
+    }
+    
+    virtual pipeline& operator<<(pipeline& p) {
+        return *this;
+    }
+    
+    virtual pipeline& operator<<(execute& e) {
+        return *this;
+    }
+    
+    virtual pipeline& operator<<(_D& d) {
+        _data[_index]=d;
+        return *this;
+    }
+   
+    
+};
+
+int main()
+{
+  pipeline<double,16> p;
+  ;
+}
