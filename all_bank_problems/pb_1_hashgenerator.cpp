b6cab45c491fe8978ec274c2ba0388921184aa49
Bank4All <94786830+Bank4All@users.noreply.github.com>
2023-02-13 23:52:46 +0100

    Create pb_1_hashgenerator.cpp

---
+++ all_bank_problems/pb_1_hashgenerator.cpp
@@ -0,0 +1,11 @@
+// Problem of Bank 1
+// Organisation  of a anonymous Market 
+// 1 market maker , animate multiple asset
+// 2 give customer a bot to run strategy
+// 3 fill order
+// 4 allow undo and unroll when possible *** VIP customer
+// 5 flash orders from external market to provide best overall prices
+// 6 remove fees on invest long term, increase fees on short term gain
+// 7 add proxy or tracker for hedging
+// 8 animate insurance pool and loan/borrow reward
+// 9 add refund leg and zero coupon guarantee
